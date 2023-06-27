8fa93158fbc979fc0f8e21c04ae9bdbe9d22ba93
Alfredo Lillo <alphonselillo@gmail.com>
2023-06-27 18:21:26 -0300

    Modelos terminados

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,51 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
+const { all } = require("../routes");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "pokemon",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      health: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      stroke: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      defending: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      speed: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      height: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      weight: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      created: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
